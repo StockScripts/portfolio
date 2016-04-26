fa44549cdf9d1fdbe7cf5d4e4be2323e879e5e5f
paulhoughton <paulhoughton@users.noreply.github.com>
2016-04-26 16:48:33 +0100

    beta 16

--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {HTTP_PROVIDERS, JSONP_PROVIDERS} from 'angular2/http';
 import {provide, enableProdMode} from 'angular2/core';
-import {RouteConfig, LocationStrategy, HashLocationStrategy, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {LocationStrategy, HashLocationStrategy} from 'angular2/platform/common';
 
 import AppComponent from './components/app';
 import QuoteService from './services/quote';
