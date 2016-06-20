a1c8535a6047cf6fc61fdf13dc8ca1bf41ac8df5
paulhoughton <paulhoughton@users.noreply.github.com>
2016-06-20 09:41:56 +0100

    Replace typings

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts', exclude: /node_modules/ },
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' },
       { test: /\.html$/, loader: 'raw-loader' },
       { test: /\.css$/, loader: 'raw-loader' }
     ]
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   module: {
     loaders: [
-      { test: /\.ts$/, loader: 'ts', exclude: /node_modules/ },
+      { test: /\.ts$/, loader: 'awesome-typescript-loader' },
       { test: /\.html$/, loader: 'raw-loader' },
       { test: /\.css$/, loader: 'raw-loader' }
     ]
